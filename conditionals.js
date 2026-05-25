7e9bf8a2f27128f05c872f7d34b3c635f48dd51c
Elijah736 <elijahlategan3@gmail.com>
2026-05-25 12:11:09 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+let score = 100     //Assign the variables
+let passingGrade = 60
+
+if(score>= passingGrade){    
+    console.log("You Passed");
+}else{
+    console.log("You Failed")
+}
+
+if(score == 100){
+    console.log("You got a perfect score!");
+}
